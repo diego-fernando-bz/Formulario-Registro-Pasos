e1e68e7d2d3a8fc8cf977a10b5ffea8246118954
Diego Basante <diegozambrano237@gmail.com>
2024-03-12 18:02:08 -0500

    agregue el proyecto de juego plataforma parte 2

--- Calculador/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.mycompany.calculador {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-
-    opens com.mycompany.calculador to javafx.fxml;
-    exports com.mycompany.calculador;
-}
